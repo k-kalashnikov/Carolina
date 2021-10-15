62f9c0b1d0cd024687890770991b0a800a76067c
Gitkerus <gitkerus@gmail.com>
2021-10-16 03:17:01 +0400

    3 of 19 video. Added lasts examples with new methods. .Replace .ToUpper and . ToLower. Lasts tasks for 3nd video about strings.

--- CSharp/Baisc/Program.cs
+++ CSharp/Baisc/Program.cs
@@ -13,6 +13,34 @@ namespace Baisc
 
             Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
             Console.WriteLine($"The name {secondFriend} had {secondFriend.Length} letters.");
+            {
+
+                string greeting = "    Hello World!    ";
+                Console.WriteLine($"[{greeting}]");
+
+                string trimmedGreeting = greeting.TrimStart();
+                Console.WriteLine($"[{trimmedGreeting}]");
+
+                trimmedGreeting = greeting.TrimEnd();
+                Console.WriteLine($"[{trimmedGreeting}]");
+
+                trimmedGreeting = greeting.Trim();
+                Console.WriteLine($"[{trimmedGreeting}]");
+            }
+
+            {
+                string sayHello = "Hello World!";
+                Console.WriteLine(sayHello);
+                sayHello = sayHello.Replace("Hello", "Greetings");
+                Console.WriteLine(sayHello);
+
+                Console.WriteLine(sayHello.ToUpper());
+                Console.WriteLine(sayHello.ToLower());
+            }
+
+            {
+                
+            }
         }
     }
 }
