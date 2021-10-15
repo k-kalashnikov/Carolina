f20182d4f5f85bc8349530f4f46b590a757e5d6b
Gitkerus <gitkerus@gmail.com>
2021-10-16 02:59:20 +0400

    3 of 19 video. Task with new commands no.2. Bug fixed mistakes from previous one, forgot about ; after 2nd string with Friends names

--- CSharp/Baisc/Program.cs
+++ CSharp/Baisc/Program.cs
@@ -7,9 +7,12 @@ namespace Baisc
         static void Main(string[] args)
         {
             string firstFriend = "Aleks";
-            string secondFriend = "Izera"
+            string secondFriend = "Izera";
 
             Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} had {secondFriend.Length} letters.");
         }
     }
 }
