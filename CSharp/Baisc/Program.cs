ad811345bd9cb14ab88327c9cbde854742a3d2b1
Gitkerus <gitkerus@gmail.com>
2021-10-16 03:36:02 +0400

    4 of 19 video. Add few more code lines with methods Contains StartsWith EndsWith, last tasks about strings in video course on current moment.

--- CSharp/Baisc/Program.cs
+++ CSharp/Baisc/Program.cs
@@ -39,8 +39,18 @@ namespace Baisc
             }
 
             {
-                
+                string songLyrics = "You say goodbye, and I say hello";
+
+                var result1 = songLyrics.Contains("goodbye");
+
+                Console.WriteLine(result1);
+                Console.WriteLine(songLyrics.Contains("goodbye"));
+                Console.WriteLine(songLyrics.Contains("greetings"));
+
+                Console.WriteLine(songLyrics.StartsWith("You"));
+                Console.WriteLine(songLyrics.EndsWith("hello"));
             }
+            
         }
     }
 }
