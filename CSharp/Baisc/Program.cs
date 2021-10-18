5b02ee2562d546d0cf6b6704ff8d8166974f67ad
Gitkerus <gitkerus@gmail.com>
2021-10-18 18:17:54 +0400

    8 of 19 video. Made 3 new methods with differened tasks from lesson.

--- CSharp/Baisc/Program.cs
+++ CSharp/Baisc/Program.cs
@@ -6,7 +6,7 @@ namespace Baisc
     {
         static void Main(string[] args)
         {
-            branches();
+            branches2();
 
             static void friends()
             {
@@ -144,11 +144,55 @@ namespace Baisc
             {
                 int a = 5;
                 int b = 6;
-                if (a + b > 10)
-                    Console.WriteLine("The answer is greater than 10.");
-                
 
+                bool something = a + b > 10;
+
+                if (something)
+                {
+                    Console.WriteLine("The answer is greater than 10.");
+                }
+                else
+                {
+                    Console.WriteLine("The answer is not greater than 10");
+                }
             }
+
+            static void branches2()
+            {
+                int a = 5;
+                int b = 6;
+                int c = 4;
+
+                if ((a + b + c > 10) && (a == b))
+                {
+                    Console.WriteLine("The answer is greater than 10");
+                    Console.WriteLine("And the first number is equal to the second");
+                }
+                else
+                {
+                    Console.WriteLine("The answer is not greater than 10");
+                    Console.WriteLine("Or the first number is not equal to the second");
+                }
+            }
+
+            static void branches3()
+            {
+                int a = 5;
+                int b = 6;
+                int c = 4;
+
+                if ((a + b + c > 10) || (a == b))
+                {
+                    Console.WriteLine("The answer is greater than 10");
+                    Console.WriteLine("And the first number is equal to the second");
+                }
+                else
+                {
+                    Console.WriteLine("The answer is not greater than 10");
+                    Console.WriteLine("Or the first number is not equal to the second");
+                }
+            }
+
         }
     }
 }
